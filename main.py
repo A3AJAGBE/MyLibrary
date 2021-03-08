b5d74985071b3e03324b29e2421ac70c5860b3d1
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-08 08:46:36 +0000

    Add add function

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello World!!!'
+    return render_template('index.html')
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
