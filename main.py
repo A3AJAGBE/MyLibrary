3cdaa29cd9ba3a93ff5b38e6bd4ee66ca30f3056
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-08 09:31:47 +0000

    Add books database

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///librarys-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-all_books = []
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'Title: {self.title}'
+
+
+db.create_all()
+
+# all_books = []
 
 
 @app.route('/')
 def index():
+    all_books = Books.query.all()
     return render_template('index.html', books=all_books)
 
 
 @app.route('/add', methods=["GET", "POST"])
 def add():
     if request.method == 'POST':
-        new_book = {
-            "title": request.form['book_name'],
-            "author": request.form['author'],
-            "rating": request.form['rating'],
-        }
-        all_books.append(new_book)
+        new_book = Books(
+            title=request.form['book_name'],
+            author=request.form['author'],
+            rating=request.form['rating'])
+        db.session.add(new_book)
+        db.session.commit()
+        # all_books.append(new_book)
         return redirect(url_for('index'))
     return render_template('add.html')
 
